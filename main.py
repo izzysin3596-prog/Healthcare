2a2ac6410ebc9a51c586f28a754f3aa68d1f165c
izzysin3596-prog <izzysin3596@gmail.com>
2026-02-04 19:59:52 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import base64
 
 st.title("당뇨 케어 푸드 스캐너")
 
-st.camera_input("음식을 촬영해주세요")
+img_file = st.camera_input("음식을 촬영해주세요")
+
+if img_file is not None:
+    bytes_data = img_file.getvalue()
+    base64_image = base64.b64encode(bytes_data).decode('utf-8')
+
+    st.success("이미지 변환 성공")
+
+    st.write("변환된 데이터 앞 100자 : ")
+    st.code(base64_image[:100])
