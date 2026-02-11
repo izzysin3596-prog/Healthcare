87d777bb24a616b22310b82fe8c4dbafb26b372b
izzysin3596-prog <izzysin3596@gmail.com>
2026-02-11 19:30:26 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import base64
+import google.generativeai as genai
 
 st.title("당뇨 케어 푸드 스캐너")
 
+GOOGLE_API_KEY
+genai.configure(api_key-GOOGLE_API_KEY)
 img_file = st.camera_input("음식을 촬영해주세요")
 
 if img_file is not None:
+
+    image = Image.open(img_file)
+    '''
     bytes_data = img_file.getvalue()
     base64_image = base64.b64encode(bytes_data).decode('utf-8')
+    '''
 
-    st.success("이미지 변환 성공")
+    st.image(image, caption="촬영된 음식", use_container_width=True)
+    #st.success("이미지 변환 성공")
 
-    st.write("변환된 데이터 앞 100자 : ")
-    st.code(base64_image[:100])
+    #st.write("변환된 데이터 앞 100자 : ")
+    #st.code(base64_image[:-100])
