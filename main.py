4284a496c0c5a4d0c548cd16fa0e0d2e84f5cea8
izzysin3596-prog <izzysin3596@gmail.com>
2026-08-12 17:00:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ with tab1:
                 events.append({
                     "title":title,
                     "start":start_date,
-                    "end":start_date
+                    "end":start_date,
                     "extendedProps": {"content": analysis_content} 
                 })
         except Exception as e:
