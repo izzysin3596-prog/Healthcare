5da8ef5aa202af0ee366194cdc5170aac2a52aaf
izzysin3596-prog <izzysin3596@gmail.com>
2026-09-14 18:59:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st # 스트림릿 라이브러리 추가
 # import base64 # 이미지를 텍스트로 변환 openia = GPT
 import google.generativeai as genai
-import requests, as pd
+import requests, pandas as pd
 import json, re
 from PIL import Image # Genai
 from notion_client import Client
